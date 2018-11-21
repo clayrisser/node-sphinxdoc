0bd9e7c445a876d91bc05244654712d2d362de0f
Jam Risser <jam@codejam.ninja>
2018-11-20 18:44:11 -0600

    Added paths

--- packages/cli/src/sphinx/index.js
+++ packages/cli/src/sphinx/index.js
@@ -1,3 +1,4 @@
+import fs from 'fs-extra';
 import path from 'path';
 import pkgDir from 'pkg-dir';
 import { python, pip } from 'python-env';
@@ -5,14 +6,24 @@ import { python, pip } from 'python-env';
 export default class Sphinx {
   constructor({ platform }) {
     this.platform = platform;
-    this.requirements = path.resolve(
-      pkgDir.sync(require.resolve(this.platform)),
-      'requirements.txt'
+    this.paths = {
+      platform: pkgDir.sync(require.resolve(this.platform))
+    };
+    this.paths.docs = path.resolve(this.paths.platform, 'docs');
+    this.paths.environment = path.resolve(this.paths.platform, 'environment');
+    this.paths.requiremnts = path.resolve(
+      this.paths.environment,
+      'requiremnts.txt'
     );
   }
 
   async init() {
-    await pip(['install', '-r', this.requirements]);
+    await this.loadEnvironment();
+    await pip(['install', '-r', this.paths.requirements]);
     await python('--version');
+  }
+
+  async loadEnvironment() {
+    fs.copySync(this.paths.docs, this.paths.environment);
   }
 }
