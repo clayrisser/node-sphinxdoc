546e084a431cf37fefcae3a62a43a8c2ca99b7da
Jam Risser <jam@codejam.ninja>
2018-11-21 19:57:20 -0600

    Fixed require helper

--- packages/cli/src/helpers.js
+++ packages/cli/src/helpers.js
@@ -1,5 +1,13 @@
+import path from 'path';
+import pkgDir from 'pkg-dir';
+
 function requireModule(moduleName) {
-  const module = require(moduleName);
+  const modulePath = pkgDir.sync(
+    require.resolve(moduleName, {
+      paths: [path.resolve(pkgDir.sync(process.cwd()), 'node_modules')]
+    })
+  );
+  const module = require(modulePath);
   if (module.__esModule) return module.default;
   return module;
 }
