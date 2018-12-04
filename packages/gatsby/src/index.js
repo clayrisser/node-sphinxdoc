29061025b8bb9fa3b4bef89577710ac8355ffcd4
Jam Risser <jam@codejam.ninja>
2018-12-03 21:05:02 -0600

    Added gatsby-theme

--- packages/gatsby/src/index.js
+++ packages/gatsby/src/index.js
@@ -1,3 +1,39 @@
+import crossSpawn from 'cross-spawn';
+import fs from 'fs-extra';
+import path from 'path';
+import pkgDir from 'pkg-dir';
 import { Platform } from '@sphinxdoc/core';
 
-export default class Rtd extends Platform {}
+export default class Rtd extends Platform {
+  async build() {
+    return this.gatsby(['build']);
+  }
+
+  async start() {
+    return this.gatsby(['develop']);
+  }
+
+  async gatsby(args) {
+    const gatsbyCli = path.resolve(
+      pkgDir.sync(
+        require.resolve('gatsby-cli', {
+          paths: path.resolve(pkgDir.sync(process.cwd()), 'node_modules')
+        })
+      ),
+      'lib/index.js'
+    );
+    const gatsbyTheme = pkgDir.sync(
+      require.resolve('@sphinxdoc/gatsby-theme', {
+        paths: path.resolve(pkgDir.sync(process.cwd()), 'node_modules')
+      })
+    );
+    return new Promise((resolve, reject) => {
+      const cp = crossSpawn('node', [gatsbyCli, ...args], {
+        cwd: gatsbyTheme,
+        stdio: 'inherit'
+      });
+      cp.on('close', resolve);
+      cp.on('error', reject);
+    });
+  }
+}
